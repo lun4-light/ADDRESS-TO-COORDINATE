4037c17c7cf4278a2e1e66b09d60eb6c9f8720b7
Jong-hyun <mo0nlight0@naver.com>
2022-04-07 02:36:46 +0900

    feat #3: Fixed an error that searchByQuery function did not return the result.

--- address-to-coordinate.js
+++ address-to-coordinate.js
@@ -1,29 +1,30 @@
 import axios from 'axios';
 import { KAKAOKEY } from './keys'
 
-const makeCoordinateArray = ( index, x, y ) => {
+const makeCoordinateArray = ( index, address, x, y ) => {
     const array = [];
     array.push(index);
+    array.push(address);
     array.push(x);
     array.push(y);
 
     return array;
 }
 
-const searchByQuery = async ( searchType, address ) => {
+const searchByQuery = async ( searchType, query ) => {
     const searchURI =
         "https://dapi.kakao.com/v2/local/search/" +
         searchType +
         ".json?query=" +
-        encodeURI(address);
+        encodeURI(query);
 
-    const { searchData } = await axios.get(searchURI, {
+    const { data } = await axios.get(searchURI, {
         headers: {
             Authorization: `KakaoAK ${KAKAOKEY}`,
         },
     });
 
-    return searchData;
+    return data;
 }
 
 const addressToCoordinate = async ( jsonData, busData ) => {
@@ -32,25 +33,26 @@ const addressToCoordinate = async ( jsonData, busData ) => {
     for (const trashcanData of jsonData) {
         if (trashcanData.point === "정류장(버스,택시 등)") {
             const busCoordinate = busData.get(parseInt(trashcanData.ARSID));
+
             if (busCoordinate) {
-                result.push(makeCoordinateArray(trashcanData.index, busCoordinate[0], busCoordinate[1]));
+                result.push(makeCoordinateArray(trashcanData.index, trashcanData.address, busCoordinate[0], busCoordinate[1]));
             }
         }
         else {
-            const addressSearchData = searchByQuery("address", address);
-    
-            if (!addressSearchData.documents[0]) {
-                const keywordSearchData = searchByQuery("keyword", address);
+            const addressSearchData = await searchByQuery("address", trashcanData.address);
 
+            if (!addressSearchData.documents[0]) {
+                const keywordSearchData = await searchByQuery("keyword", trashcanData.address);
+                
                 if (!keywordSearchData.documents[0]) {
-                    result.push(makeCoordinateArray(trashcanData.index, -1, -1));
+                    result.push(makeCoordinateArray(trashcanData.index, trashcanData.address,-1, -1));
                 }
                 else {
-                    result.push(makeCoordinateArray(trashcanData.index, keywordSearchData.documents[0].x, keywordSearchData.documents[0].y));
+                    result.push(makeCoordinateArray(trashcanData.index, trashcanData.address, keywordSearchData.documents[0].x, keywordSearchData.documents[0].y));
                 }
             }
             else {
-                result.push(makeCoordinateArray(trashcanData.index, addrSearchData.documents[0].x, addrSearchData.documents[0].y)); 
+                result.push(makeCoordinateArray(trashcanData.index, trashcanData.address, addressSearchData.documents[0].x, addressSearchData.documents[0].y)); 
             }
         }
     }
